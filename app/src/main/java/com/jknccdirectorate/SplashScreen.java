9fac7d5c53f4c45abb05e8a29f5adc5a05ea57fa
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2017-09-07 17:55:31 +0530

    Splash screen added

--- app/src/main/java/com/jknccdirectorate/SplashScreen.java
+++ app/src/main/java/com/jknccdirectorate/SplashScreen.java
@@ -1,13 +1,41 @@
 package com.jknccdirectorate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        nextActivity();
+    }
+
+    private void nextActivity() {
+        intent = new Intent(SplashScreen.this, MainActivity.class);
+        Thread thread = new Thread() {
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
+
