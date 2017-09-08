66a63345335acb5773272f93c2b54723af2d35b3
Balpreet <balpreets10@gmail.com>
2017-09-09 01:29:18 +0530

    Added Home Fragment

--- app/src/main/java/com/jknccdirectorate/MainActivity.java
+++ app/src/main/java/com/jknccdirectorate/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListView;
 
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     List<ExpandedMenuModel> listDataHeader;
     HashMap<ExpandedMenuModel, List<String>> listDataChild;
     DrawerLayout drawer;
+    final String TAG = "Main";
 
 
     @Override
@@ -170,6 +172,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        expandableList.setSelectedGroup(0);
+        expandableList.setSelection(0);
+        expandableList.setSelectedChild(0,0,true);
+        Log.d(TAG,"Position = " + expandableList.getSelectedItem());
     }
 
 
