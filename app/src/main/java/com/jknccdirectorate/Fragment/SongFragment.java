004d2620489d4f1a30a9d909719706cd0d9206c1
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2017-09-13 21:17:42 +0530

    Aim and Song added

--- app/src/main/java/com/jknccdirectorate/Fragment/SongFragment.java
+++ app/src/main/java/com/jknccdirectorate/Fragment/SongFragment.java
@@ -1,11 +1,14 @@
 package com.jknccdirectorate.Fragment;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jknccdirectorate.Activity.MainActivity;
 import com.jknccdirectorate.R;
@@ -24,9 +27,16 @@ public class SongFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_song, container, false);
+        View view = inflater.inflate(R.layout.fragment_song, container, false);
+        TextView hindi = (TextView) view.findViewById(R.id.hindiSong);
+        TextView english = (TextView) view.findViewById(R.id.englishSong);
+        Typeface fontHindi = Typeface.createFromAsset(getContext().getAssets(), "MANGAL.TTF");
+        hindi.setTypeface(fontHindi);
+        hindi.setMovementMethod(new ScrollingMovementMethod());
+        english.setMovementMethod(new ScrollingMovementMethod());
+        return view;
     }
+
     public void onResume() {
         super.onResume();
         ((MainActivity) getActivity()).setActionBarTitle(getResources().getString(R.string.song));
