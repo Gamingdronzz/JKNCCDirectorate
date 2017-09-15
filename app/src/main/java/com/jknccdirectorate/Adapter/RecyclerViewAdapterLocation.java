a27938a28a9c36811dc698cb8a61491d0de31cf9
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2017-09-15 18:19:45 +0530

    Locations added

---
+++ app/src/main/java/com/jknccdirectorate/Adapter/RecyclerViewAdapterLocation.java
@@ -0,0 +1,51 @@
+package com.jknccdirectorate.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jknccdirectorate.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hp on 15-09-2017.
+ */
+
+public class RecyclerViewAdapterLocation extends RecyclerView.Adapter<RecyclerViewAdapterLocation.MyViewHolder> {
+
+    ArrayList<String> locationArray;
+
+    public RecyclerViewAdapterLocation(ArrayList<String> locationArray) {
+        this.locationArray = locationArray;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        MyViewHolder myViewHolder = new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_locations, parent, false));
+        return myViewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        String location = locationArray.get(position);
+        holder.tv.setText(location);
+    }
+
+    @Override
+    public int getItemCount() {
+        return locationArray.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView tv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.tv = (TextView) itemView.findViewById(R.id.loc);
+        }
+    }
+}
--- app/src/main/java/com/jknccdirectorate/Fragment/ContactFragment.java
+++ app/src/main/java/com/jknccdirectorate/Fragment/ContactFragment.java
@@ -1,32 +1,123 @@
 package com.jknccdirectorate.Fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jknccdirectorate.Activity.MainActivity;
+import com.jknccdirectorate.Adapter.RecyclerViewAdapterLocation;
 import com.jknccdirectorate.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+
 public class ContactFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    RecyclerViewAdapterLocation adapter;
+    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Double> latitude = new ArrayList<>();
+    ArrayList<Double> longitude = new ArrayList<>();
 
     public ContactFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.locations);
+        list = getLocationList();
+        latitude = getLatList();
+        longitude = getLongList();
+        adapter = new RecyclerViewAdapterLocation(list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+            GestureDetector gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+            });
+
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                if (child != null && gestureDetector.onTouchEvent(e)) {
+                    int pos = rv.getChildAdapterPosition(child);
+                    double lat = latitude.get(pos);
+                    double log = longitude.get(pos);
+                    // String uri = String.format(Locale.ENGLISH, "geo:%f,%f?q=%s", lat, log ,Uri.encode(list.get(pos)));
+                    Uri uri = Uri.parse("geo:0,0?q=" + lat + "," + log + "(" + Uri.encode(list.get(pos)) + ")");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    getContext().startActivity(intent);
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+        return view;
     }
+
+    private ArrayList<Double> getLatList() {
+        latitude.add(32.7253156);
+        latitude.add(32.712113);
+        latitude.add(34.0621045);
+        latitude.add(32.7002194);
+        latitude.add(32.7051315);
+        latitude.add(32.7051315);
+        latitude.add(33.76753);
+        latitude.add(32.9837178);
+        latitude.add(32.7857434);
+        return latitude;
+    }
+
+    private ArrayList<Double> getLongList() {
+        longitude.add(74.8412983);
+        longitude.add(74.8622234);
+        longitude.add(74.8019077);
+        longitude.add(74.8644794);
+        longitude.add(74.8631721);
+        longitude.add(74.8631721);
+        longitude.add(74.1000921);
+        longitude.add(75.7103464);
+        longitude.add(74.9214256);
+        return longitude;
+    }
+
+    private ArrayList<String> getLocationList() {
+        list.add("NCC Directorate, J&K");
+        list.add("Group Headquarters, Jammu");
+        list.add("Group Headquarters, Srinagar");
+        list.add("2 J&K Battalion NCC Jammu");
+        list.add("2 J&K Girls Battalion NCC Jammu");
+        list.add("1 J&K Armed Squadron NCC Jammu");
+        list.add("1 J&K Artillery Battalion NCC  Poonch");
+        list.add("2 J&K (I) Coy  NCC Bhaderwah");
+        list.add("JD (I) Coy NCC  SS Nagrota ");
+        return list;
+    }
+
     @Override
     public void onResume() {
         super.onResume();
