0a8c4ed5c361bc835b83bf9c1a89cda29d8580ba
Gunjita-Jamwal <gunjita.jamwal@gmail.com>
2017-11-28 17:32:56 +0530

    Pledge fragment modified

--- app/src/main/java/com/jknccdirectorate/Fragment/PledgeFragment.java
+++ app/src/main/java/com/jknccdirectorate/Fragment/PledgeFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 import com.jknccdirectorate.Activity.MainActivity;
 import com.jknccdirectorate.R;
@@ -14,6 +17,9 @@ import com.jknccdirectorate.R;
  * A simple {@link Fragment} subclass.
  */
 public class PledgeFragment extends Fragment {
+
+    Animation animationTranslate1, animationTranslate2, animationTranslate3;
+    LinearLayout layout1, layout2, layout3;
 
 
     public PledgeFragment() {
@@ -24,9 +30,67 @@ public class PledgeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pledge, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_pledge, container, false);
+        layout1 = (LinearLayout) view.findViewById(R.id.pledge1);
+        layout2 = (LinearLayout) view.findViewById(R.id.pledge2);
+        layout3 = (LinearLayout) view.findViewById(R.id.pledge3);
+        layout1.setVisibility(View.INVISIBLE);
+        layout2.setVisibility(View.INVISIBLE);
+        layout3.setVisibility(View.INVISIBLE);
+        setupAnimations(view);
+        return view;
     }
+
+    private void setupAnimations(View view) {
+        animationTranslate1 = AnimationUtils.loadAnimation(view.getContext(), R.anim.move);
+        animationTranslate2 = AnimationUtils.loadAnimation(view.getContext(), R.anim.move);
+        animationTranslate3 = AnimationUtils.loadAnimation(view.getContext(), R.anim.move);
+        animationTranslate1.reset();
+        animationTranslate2.reset();
+        animationTranslate3.reset();
+        layout1.setVisibility(View.VISIBLE);
+
+        layout1.startAnimation(animationTranslate1);
+        animationTranslate1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                layout2.setVisibility(View.VISIBLE);
+
+                layout2.startAnimation(animationTranslate2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        animationTranslate2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                layout3.setVisibility(View.VISIBLE);
+                layout3.startAnimation(animationTranslate3);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+
     public void onResume() {
         super.onResume();
         ((MainActivity) getActivity()).setActionBarTitle(getResources().getString(R.string.pledge));
